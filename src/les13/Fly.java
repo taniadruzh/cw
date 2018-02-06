54a70932c8cd660c6ceb284c0df6aad38d5e68af
taniadruzh <tania.druzhchenko@gmail.com>
2018-02-06 20:29:15 +0200

    interface

---
+++ src/les13/Fly.java
@@ -0,0 +1,8 @@
+package les13;
+
+/**
+ * Created by java on 06.02.2018.
+ */
+public interface Fly {
+    public void fly();
+}
---
+++ src/les13/RubberDuck.java
@@ -0,0 +1,12 @@
+package les13;
+
+/**
+ * Created by java on 06.02.2018.
+ */
+public class RubberDuck implements Fly {
+    @Override
+    public void fly() {
+
+    }
+
+}

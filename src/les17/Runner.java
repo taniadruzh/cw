13e6b22006f213a81df567225a1c5313106ef026
taniadruzh <tania.druzhchenko@gmail.com>
2018-03-27 20:15:19 +0300

    les17

--- src/les17/Runner.java
+++ src/les17/Runner.java
@@ -17,6 +17,7 @@ public class Runner {
     public static void main(String[] args) throws IOException {
         createDir();
         checkdir();
+        t2();
     }
 
     public static void createDir(){
@@ -57,6 +58,9 @@ public class Runner {
                 } finally {
                     is.close();
                     os.close();
+                    if(item.delete()){
+                        System.out.println(" файл был удален");
+                    }else System.out.println("Файл не был найден");
                 }
             }
         }
@@ -75,11 +79,71 @@ public class Runner {
                     while ((length = is.read(buffer)) > 0) {
                         os.write(buffer, 0, length);
                     }
+                    item.delete();
                 } finally {
                     is.close();
                     os.close();
+                    if(item.delete()){
+                        System.out.println(" файл был удален");
+                    }else System.out.println("Файл не был найден");
                 }
             }
+        }
+    }
+
+    public static void t2() throws IOException {
+        File file1 = new File(dir1,"text1.txt");
+        File file2 = new File(dir1,"text2.txt");
+        File file3 = new File(dir1,"text3.txt");
+
+        String text1 = "- Скажи-ка, дядя, ведь не даром\n" +
+                "Москва, спаленная пожаром,\n" +
+                "Французу отдана?";
+        String text2 = "Наедине с тобой, брат,\n" +
+                "Хотел бы я побыть:\n" +
+                "На свете мало, говорят,\n" +
+                "Мне остается жить!";
+        String text3 = "Белеет парус одинокий\n" +
+                "В тумане моря голубом!...\n" +
+                "Что ищет он в стране далекой?\n" +
+                "Что кинул он в краю родном?...";
+
+        file1.createNewFile();
+        file2.createNewFile();
+        file3.createNewFile();
+
+        try(FileOutputStream fos=new FileOutputStream(file1))
+        {
+            // перевод строки в байты
+            byte[] buffer = text1.getBytes();
+
+            fos.write(buffer, 0, buffer.length);
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+        try(FileOutputStream fos=new FileOutputStream(file2))
+        {
+            // перевод строки в байты
+            byte[] buffer = text2.getBytes();
+
+            fos.write(buffer, 0, buffer.length);
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+        try(FileOutputStream fos=new FileOutputStream(file3))
+        {
+            // перевод строки в байты
+            byte[] buffer = text3.getBytes();
+
+            fos.write(buffer, 0, buffer.length);
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
         }
     }
 }

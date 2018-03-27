4800793cf0919e4a8bec8864e86b95ac08126685
taniadruzh <tania.druzhchenko@gmail.com>
2018-03-27 20:53:22 +0300

    les17

--- src/les17/Runner.java
+++ src/les17/Runner.java
@@ -1,8 +1,6 @@
 package les17;
 
 
-import org.omg.CORBA.Environment;
-
 import java.io.*;
 
 
@@ -11,9 +9,9 @@ import java.io.*;
  * Created by java on 27.03.2018.
  */
 public class Runner {
-    public static File dir1;
-    public static File dir2;
-    public static File dir3;
+    public static File workDir;
+    public static File errorDir;
+    public static File doneDir;
 
     public static File file1;
     public static File file2;
@@ -26,76 +24,62 @@ public class Runner {
         t3();
     }
 
+    public static void moveFile(File from, File to) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(from);
+            os = new FileOutputStream(new File(to,from.getName()));
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+            }
+        } finally {
+            is.close();
+            os.close();
+            if(from.delete()){
+                System.out.println(" файл был удален");
+            }else System.out.println("Файл не был найден");
+        }
+    }
+
     /*Написать программу которая создает 3 пустых папки.Их имена: "work", "done", "error".*/
     public static void createDir(){
         // создаем объект File для каталога
-        dir1 = new File("D://_aJavaBestTeam//project//untitled3//work");
-        boolean created = dir1.mkdir();
+        workDir = new File("D://_aJavaBestTeam//project//untitled3//work");
+        boolean created = workDir.mkdir();
         if(created)
             System.out.println("Каталог успешно создан");
-        dir2 = new File("D://_aJavaBestTeam//project//untitled3//error");
-        created = dir2.mkdir();
+        errorDir = new File("D://_aJavaBestTeam//project//untitled3//error");
+        created = errorDir.mkdir();
         if(created)
             System.out.println("Каталог успешно создан");
-        dir3 = new File("D://_aJavaBestTeam//project//untitled3//done");
-        created = dir3.mkdir();
+        doneDir = new File("D://_aJavaBestTeam//project//untitled3//done");
+        created = doneDir.mkdir();
         if(created)
             System.out.println("Каталог успешно создан");
         // создаем объекты для файлов, которые находятся в каталоге
-//        File file1 = new File("C://SomeDir", "Hello.txt");
-//        File file2 = new File(dir1, "Hello2.txt");
+
     }
 
     /*Сделать проверку - если в папке "done" или "erro" есть какие то файлы перенести их в "work".
     Остальные две должны остаться пустыми.*/
     public static void checkdir() throws IOException {
-        if(dir2.isDirectory())
+        if(errorDir.isDirectory())
         {
             // получаем все вложенные объекты в каталоге
-            for(File item : dir2.listFiles()){
+            for(File item : errorDir.listFiles()){
 
-                InputStream is = null;
-                OutputStream os = null;
-                try {
-                    is = new FileInputStream(item);
-                    os = new FileOutputStream(new File(dir1,item.getName()));
-                    byte[] buffer = new byte[1024];
-                    int length;
-                    while ((length = is.read(buffer)) > 0) {
-                        os.write(buffer, 0, length);
-                    }
-                } finally {
-                    is.close();
-                    os.close();
-                    if(item.delete()){
-                        System.out.println(" файл был удален");
-                    }else System.out.println("Файл не был найден");
-                }
+                moveFile(item, workDir);
             }
         }
-        if(dir3.isDirectory())
+        if(doneDir.isDirectory())
         {
             // получаем все вложенные объекты в каталоге
-            for(File item : dir3.listFiles()){
+            for(File item : doneDir.listFiles()){
 
-                InputStream is = null;
-                OutputStream os = null;
-                try {
-                    is = new FileInputStream(item);
-                    os = new FileOutputStream(new File(dir1,item.getName()));
-                    byte[] buffer = new byte[1024];
-                    int length;
-                    while ((length = is.read(buffer)) > 0) {
-                        os.write(buffer, 0, length);
-                    }
-                    item.delete();
-                } finally {
-                    is.close();
-                    os.close();
-                    if(item.delete()){
-                        System.out.println(" файл был удален");
-                    }else System.out.println("Файл не был найден");
-                }
+                moveFile(item, workDir);
             }
         }
     }
@@ -105,9 +89,9 @@ public class Runner {
     При чем, стихотворение берем из интернета любое и записываем в константу.
     После чего работаем с этой константой и пишем 1-ю строку в первый файл, вторую во второй и  т.д.*/
     public static void t2() throws IOException {
-        file1 = new File(dir1,"text1.txt");
-        file2 = new File(dir1,"text2.txt");
-        file3 = new File(dir1,"text3.txt");
+        file1 = new File(workDir,"text1.txt");
+        file2 = new File(workDir,"text2.txt");
+        file3 = new File(workDir,"text3.txt");
 
         String text1 = "I like you well, O trusty dagger mine,\n" +
                 "My comrade wrought of cool Damascus steel!\n" +
@@ -117,45 +101,26 @@ public class Runner {
                 "The land of slaves, the land of lords:\n" +
                 "And you, the blue-uniformed ushers,\n" +
                 "And people who worship them as gods.";
-        String text3 = "In noon's heat, in a dale of Dagestan\n" +
-                "With lead inside my breast, stirless I lay;\n" +
-                "The deep wound still smoked on; my blood\n" +
-                "Kept trickling drop by drop away.";
+        String text3 = "Innoon'sheat,inadaleofDagestan";
 
         file1.createNewFile();
         file2.createNewFile();
         file3.createNewFile();
 
-        try(FileOutputStream fos=new FileOutputStream(file1))
+        createFile2(file1,text1);
+        createFile2(file2,text2);
+        createFile2(file3,text3);
+
+
+    }
+
+    public static void createFile2(File file, String str){
+        try(FileOutputStream fos=new FileOutputStream(file))
         {
             // перевод строки в байты
-            byte[] buffer = text1.getBytes();
+            byte[] buffer = str.getBytes();
 
             fos.write(buffer, 0, buffer.length);
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
-        try(FileOutputStream fos=new FileOutputStream(file2))
-        {
-            // перевод строки в байты
-            byte[] buffer = text2.getBytes();
-
-            fos.write(buffer, 0, buffer.length);
-            fos.close();
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
-        try(FileOutputStream fos=new FileOutputStream(file3))
-        {
-            // перевод строки в байты
-            byte[] buffer = text3.getBytes();
-
-            fos.write(buffer, 0, buffer.length);
-            fos.close();
         }
         catch(IOException ex){
 
@@ -168,6 +133,33 @@ public class Runner {
     В противоположном случае в "done".
     Для этого на предыдущем этапе в один из файлов записать строку без пробелов.*/
     public static void t3(){
+        checkFile3(file1);
+        checkFile3(file2);
+        checkFile3(file3);
+    }
 
+
+
+    public static void checkFile3(File file){
+        try(FileInputStream fin=new FileInputStream(file))
+        {
+            int i = -1;
+            boolean hasSpase = false;
+            while((i=fin.read())!=-1){
+                if((char)i == ' ') {
+                    moveFile(file,doneDir);
+                    hasSpase = true;
+                    break;
+                }
+            }
+            if (!hasSpase) {
+                moveFile(file,errorDir);
+            }
+            fin.close();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
     }
 }

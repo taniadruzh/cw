d17d056a78970bc97c1d2feceb9ae7363b49d132
taniaDruzh <tania.druzhchenko@gmail.com>
2018-04-20 20:56:56 +0300

    lom

--- src/lesLom/lesLom.java
+++ src/lesLom/lesLom.java
@@ -1,8 +1,18 @@
 package lesLom;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by java on 20.04.2018.
  */
-@Data
 public class lesLom {
+    public static void main(String[] args) {
+
+        String input = "Tania tania ";
+        Pattern pattern = Pattern.compile("Tania");
+        Matcher matcher = pattern.matcher(input);
+        while(matcher.find())
+            System.out.println(matcher.group());
+    }
 }
